a5c3d3be20cbca1fb0d5084b153dd9d792dcd309
Yogendra Manawat <yogendramanawat@gmail.com>
2023-03-20 11:24:11 +0530

    updated css for diagnose page

--- src/components/DiagnoseHome.js
+++ src/components/DiagnoseHome.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
-import "../App.css";
+import "./DiagnoseHome.css";
 
 import React, { useState } from "react";
 
