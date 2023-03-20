d1f213d2483a257ab2280c5fa21c9e0f76a8b03f
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-03-20 12:34:55 +0530

    sign up button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Navigation.css';
+import React from "react";
+import "./Navigation.css";
 
 function Navigation(props) {
   const { currentPage, onClick } = props;
@@ -7,16 +7,28 @@ function Navigation(props) {
     <nav className="navigation">
       <div className="logo">DOCTOR.AI</div>
       <ul className="nav-links">
-        <li className={currentPage === 'home' ? 'active' : ''} onClick={() => onClick('home')}>
+        <li
+          className={currentPage === "home" ? "active" : ""}
+          onClick={() => onClick("home")}
+        >
           Home
         </li>
-        <li className={currentPage === 'diagnose' ? 'active' : ''} onClick={() => onClick('diagnose')}>
+        <li
+          className={currentPage === "diagnose" ? "active" : ""}
+          onClick={() => onClick("diagnose")}
+        >
           Diagnostic Images
         </li>
-        <li className={currentPage === 'report' ? 'active' : ''} onClick={() => onClick('report')}>
+        <li
+          className={currentPage === "report" ? "active" : ""}
+          onClick={() => onClick("report")}
+        >
           Blood Test
         </li>
       </ul>
+      <div className="sign_up">
+        <button>SIGN UP</button>
+      </div>
     </nav>
   );
 }
